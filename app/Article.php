2287208f5b617e5f3efd96506167f8ac715b1df1
Vinicius Marques <vncsmrqs@gmail.com>
2019-02-12 08:39:17 -0200

    Lesson 21 - Many to Many Relations (With Tags)

--- app/Article.php
+++ app/Article.php
@@ -44,5 +44,10 @@ class Article extends Model
     {
         return $this->belongsTo('App\User');
     }
+    
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
 
 }
